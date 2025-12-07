8b5d3e96f16f2cb5bb90b73113e27832429b60aa
LockOriginalMods <81614998+LockOriginalMods@users.noreply.github.com>
2025-12-07 15:12:02 +0300

    Add files via upload

---
+++ src/main/java/com/desx/MailoAdditionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.desx;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MailoAdditionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
